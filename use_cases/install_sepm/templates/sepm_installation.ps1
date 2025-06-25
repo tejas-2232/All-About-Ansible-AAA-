f9304e8120ffd7e1156d872b08f5e0a7aea97a88
Tejas Bachhav <tejasbachhav98@gmail.com>
2025-06-24 23:57:41 -0400

    Update sepm_installation.ps1

--- use_cases/install_sepm/templates/sepm_installation.ps1
+++ use_cases/install_sepm/templates/sepm_installation.ps1
@@ -1,1 +1,6 @@
 Start-Process -FilePath 'C:\SEPM-Win\setup.exe' -Verb runAs -Wait
+
+Start-Sleep -s 72
+
+$serviceName = "Symantec Endpoint Protection"
+$software = "Symantec Endpoint Protection version 14.0.1032.0200 - English";
